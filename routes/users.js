b5ba90425f2b938f1a68ceae080f2f6fdc0fdaca
Ander Berruezo Aguirre <berru0429@gmail.com>
2023-11-20 22:15:20 +0100

    borra

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,71 @@
 var express = require('express');
 var router = express.Router();
-
+var mongojs = require('mongojs')
+var db = mongojs('usersdb', ['users'])
+/*
 let users = [
   {id: Date.now(), izena: "John", abizena: "Doe", email: "john@doe.com"},
 ];
+*/
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render("users", {
-    title: "Users", 
-    users: users
+  db.users.find(function(err, users) {
+    if (err) {
+      console.log(err)
+  } else {
+    res.render("users", {
+      title: "Users", 
+      users: users
+    });
+  }
   });
+  
 });
 
 router.get('/list', function(req, res, next) {
-  res.json(users)
+  db.users.find(function(err, users) {
+    if (err) {
+      console.log(err)
+  } else {
+    res.json(users)
+  }
+  });
   });
 
 
 router.post("/new", (req, res) => {
-  users.push(req.body);
-  res.json(users);
+  let user = {
+    id: req.body.id,
+    izena: req.body.izena,
+    abizena: req.body.abizena,
+    email: req.body.email
+  };
+
+  db.users.insert( user );
+  /*
+  db.users.find(function(err, users) {
+    if (err) {
+      console.log(err)
+  } else {
+    res.json(users)
+  }
+  });
+  */
 });
 
 router.delete("/delete/:id", (req, res) => {
-  users = users.filter(user => user.id != req.params.id);
-  res.json(users);
+
+  db.users.remove({id: parseInt(req.params.id)});
+  /*
+  db.users.find(function(err, users) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json(users)
+    }
+    });
+    */
 });
 
 router.put("/update/:id", (req, res) => {
