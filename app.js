81ed5f7f33ab16dee5b9556e7979afac53dc06a7
Ander Berruezo Aguirre <berru0429@gmail.com>
2023-12-19 16:26:18 +0100

    push

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/styles/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
-app.use('/uploads', express.static('uploads'));
-app.use('/nofoto', express.static('nofoto'));
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
+app.use('/nofoto', express.static(path.join(__dirname,'nofoto')));
 //app.use('/errorfoto', express.static(path.join(__dirname, 'errorfoto')));
 
 app.use('/', indexRouter);
